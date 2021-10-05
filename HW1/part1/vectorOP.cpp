d29192958546aef7becdfb20114baeca2065c165
steven112163 <steven112163@gmail.com>
2021-10-05 17:17:27 +0800

    Implement part 1

--- HW1/part1/vectorOP.cpp
+++ HW1/part1/vectorOP.cpp
@@ -46,7 +46,104 @@ void clampedExpVector(float *values, int *exponents, float *output, int N) {
     // Your solution should work for any value of
     // N and VECTOR_WIDTH, not just when VECTOR_WIDTH divides N
     //
-    for (int i = 0; i < N; i += VECTOR_WIDTH) {
+    __pp_vec_float x, result;
+    __pp_vec_int y, count;
+    __pp_vec_int zero = _pp_vset_int(0);
+    __pp_vec_int int_one = _pp_vset_int(1);
+    __pp_vec_float float_one = _pp_vset_float(1.f);
+    __pp_vec_float nine = _pp_vset_float(9.999999f);
+
+    __pp_mask maskAll = _pp_init_ones();
+    __pp_mask maskIsZero, maskIsNotZero, maskGtNine, maskPart;
+
+    int i;
+    for (i = 0; i < N; i += VECTOR_WIDTH) {
+        // Handle (N % VECTOR_WIDTH) != 0
+        if (i + VECTOR_WIDTH > N)
+            break;
+
+        // Load vector of values from contiguous memory addresses
+        _pp_vload_float(x, values + i, maskAll); // x = values[i];
+        // Load vector of exponents from contiguous memory addresses
+        _pp_vload_int(y, exponents + i, maskAll); // y = exponents[i];
+
+        // Set mask according to predicate
+        _pp_veq_int(maskIsZero, y, zero, maskAll); // if (y == 0) {
+
+        // Execute instruction ("if" clause)
+        _pp_vmove_float(result, float_one, maskIsZero); // output[i] = 1.f;
+
+        // Inverse maskIsZero to generate "else" mask
+        maskIsNotZero = _pp_mask_not(maskIsZero); // } else {
+
+        // Execute instruction ("else" clause)
+        // Move x to result
+        _pp_vmove_float(result, x, maskIsNotZero); // result = x;
+        // Get count
+        _pp_vsub_int(count, y, int_one, maskIsNotZero); // count = y - 1;
+        // Get new non-zero mask
+        _pp_vgt_int(maskIsNotZero, count, zero, maskIsNotZero);
+        // While loop
+        while (_pp_cntbits(maskIsNotZero) > 0) { // while (count > 0) {
+            // Multiply result by x
+            _pp_vmult_float(result, result, x, maskIsNotZero); // result *= x;
+            // Subtract count by 1
+            _pp_vsub_int(count, count, int_one, maskIsNotZero); // count--;
+            // Get new non-zero mask
+            _pp_vgt_int(maskIsNotZero, count, zero, maskIsNotZero);
+        }
+        // Set mask according to predicate
+        _pp_vgt_float(maskGtNine, result, nine, maskAll); // if (result > 9.999999f) {
+        // Execute instruction ("if" clause)
+        _pp_vmove_float(result, nine, maskGtNine); // result = 9.999999f;
+
+        // Write results back to memory
+        _pp_vstore_float(output + i, result, maskAll); // output[i] = result;
+    }
+
+    // Handle (N % VECTOR_WIDTH) != 0
+    if (i + VECTOR_WIDTH > N) {
+        maskPart = _pp_init_ones(N - i);
+        maskIsZero = _pp_init_ones(0);
+
+        // Load vector of values from contiguous memory addresses
+        _pp_vload_float(x, values + i, maskPart); // x = values[i];
+        // Load vector of exponents from contiguous memory addresses
+        _pp_vload_int(y, exponents + i, maskPart); // y = exponents[i];
+
+        // Set mask according to predicate
+        _pp_veq_int(maskIsZero, y, zero, maskPart); // if (y == 0) {
+
+        // Execute instruction ("if" clause)
+        _pp_vmove_float(result, float_one, maskIsZero); // output[i] = 1.f;
+
+        // Inverse maskIsZero to generate "else" mask
+        maskIsNotZero = _pp_mask_not(maskIsZero); // } else {
+        maskIsNotZero = _pp_mask_and(maskIsNotZero, maskPart);
+
+        // Execute instruction ("else" clause)
+        // Move x to result
+        _pp_vmove_float(result, x, maskIsNotZero); // result = x;
+        // Get count
+        _pp_vsub_int(count, y, int_one, maskIsNotZero); // count = y - 1;
+        // Get new non-zero mask
+        _pp_vgt_int(maskIsNotZero, count, zero, maskIsNotZero);
+        // While loop
+        while (_pp_cntbits(maskIsNotZero) > 0) { // while (count > 0) {
+            // Multiply result by x
+            _pp_vmult_float(result, result, x, maskIsNotZero); // result *= x;
+            // Subtract count by 1
+            _pp_vsub_int(count, count, int_one, maskIsNotZero); // count--;
+            // Get new non-zero mask
+            _pp_vgt_int(maskIsNotZero, count, zero, maskIsNotZero);
+        }
+        // Set mask according to predicate
+        _pp_vgt_float(maskGtNine, result, nine, maskPart); // if (result > 9.999999f) {
+        // Execute instruction ("if" clause)
+        _pp_vmove_float(result, nine, maskGtNine); // result = 9.999999f;
+
+        // Write results back to memory
+        _pp_vstore_float(output + i, result, maskPart); // output[i] = result;
     }
 }
 
