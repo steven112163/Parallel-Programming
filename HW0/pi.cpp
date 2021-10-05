c0d25339b0a3a25dc7a8c284b0a43787fb90de30
steven112163 <steven112163@gmail.com>
2021-10-05 14:49:57 +0800

    Implement hw0 in cpp

--- HW0/pi.cpp
+++ HW0/pi.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+
+#define NUMBER_OF_TOSSES 1000000
 
 
 int main(void) {
+    std::srand(std::time(NULL));
+
+    long long number_in_circle = 0;
+    for (long long toss = 0; toss < NUMBER_OF_TOSSES; toss++) {
+        double x = std::rand() / ((double) RAND_MAX) * 2 - 1;
+        double y = std::rand() / ((double) RAND_MAX) * 2 - 1;
+        if (x * x + y * y <= 1)
+            number_in_circle++;
+    }
+    std::cout << std::setprecision(3) << 4 * number_in_circle / ((double) NUMBER_OF_TOSSES) << std::endl;
+
     return 0;
 }
