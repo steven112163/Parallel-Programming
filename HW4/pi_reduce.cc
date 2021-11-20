39ed78d94ed20848898ee8b32212c22afdcd8181
steven112163 <steven112163@gmail.com>
2021-11-20 23:15:07 +0800

    Implement 1.2.5 of hw4

--- HW4/pi_reduce.cc
+++ HW4/pi_reduce.cc
@@ -15,11 +15,30 @@ int main(int argc, char **argv) {
     // ---
 
     // TODO: MPI init
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    // Start tossing
+    unsigned int seed = (unsigned int) (world_rank + 1) * time(NULL);
+
+    double max_div_2 = (double) RAND_MAX / 2;
+    long long num_of_tosses = (long long) tosses / world_size;
+    long long num_in_circle = 0;
+
+    for (long long toss = 0; toss < num_of_tosses; toss++) {
+        double x = (double) rand_r(&seed) / max_div_2 - 1;
+        double y = (double) rand_r(&seed) / max_div_2 - 1;
+        if (x * x + y * y <= 1)
+            num_in_circle++;
+    }
 
     // TODO: use MPI_Reduce
+    long long result;
+    MPI_Reduce(&num_in_circle, &result, 1, MPI_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
 
     if (world_rank == 0) {
         // TODO: PI result
+        pi_result = 4.0 * result / ((double) tosses);
 
         // --- DON'T TOUCH ---
         double end_time = MPI_Wtime();
