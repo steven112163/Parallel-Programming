3e5db382c0b3e7f366ed0ef603d524afe2003979
steven112163 <steven112163@gmail.com>
2021-11-20 21:54:43 +0800

    Implement 1.2.1 of hw4

--- HW4/pi_block_linear.cc
+++ HW4/pi_block_linear.cc
@@ -15,15 +15,40 @@ int main(int argc, char **argv) {
     // ---
 
     // TODO: init MPI
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    MPI_Status status;
+    int tag = 0;
+
+    // Start tossing
+    unsigned int seed = (unsigned int) (world_rank + 1) * time(NULL);
+
+    double max_div_2 = (double) RAND_MAX / 2;
+    long long num_of_tosses = (long long) tosses / world_size;
+    long long num_in_circle = 0;
+
+    for (long long toss = 0; toss < num_of_tosses; toss++) {
+        double x = (double) rand_r(&seed) / max_div_2 - 1;
+        double y = (double) rand_r(&seed) / max_div_2 - 1;
+        if (x * x + y * y <= 1)
+            num_in_circle++;
+    }
 
     if (world_rank > 0) {
         // TODO: handle workers
+        MPI_Send(&num_in_circle, 1, MPI_LONG, 0, tag, MPI_COMM_WORLD);
     } else if (world_rank == 0) {
         // TODO: master
+        long long received_num_in_circle;
+        for (int source = 1; source < world_size; source++) {
+            MPI_Recv(&received_num_in_circle, 1, MPI_LONG, source, tag, MPI_COMM_WORLD, &status);
+            num_in_circle += received_num_in_circle;
+        }
     }
 
     if (world_rank == 0) {
         // TODO: process PI result
+        pi_result = 4.0 * num_in_circle / ((double) tosses);
 
         // --- DON'T TOUCH ---
         double end_time = MPI_Wtime();
